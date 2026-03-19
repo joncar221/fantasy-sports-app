617e9e8a8310c4d87758c3b0ae2ec1cddb1482fe
joncar221 <joncar@udel.edu>
2026-03-19 00:39:21 -0400

    backend change

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const { PrismaClient } = require('@prisma/client');
 const cors = require('cors');
 
 app.use(express.json());
