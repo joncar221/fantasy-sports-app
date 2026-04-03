2446689f6e7eae7fd82920f19061e6f1c8822555
joncar221 <joncar@udel.edu>
2026-04-02 22:59:03 -0400

    Updated create league page design

--- src/app/create-league-page/create-league-page.ts
+++ src/app/create-league-page/create-league-page.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HeaderBar } from '../header-bar/header-bar';
+import { SidebarService } from '../sidebar-service';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-create-league-page',
-  imports: [],
+  imports: [HeaderBar, Sidebar],
   templateUrl: './create-league-page.html',
   styleUrl: './create-league-page.css',
 })
-export class CreateLeaguePage {}
+export class CreateLeaguePage {
+  protected sidebarService = inject(SidebarService);
+}
